690db1c72362690c57939e7341688f49b1adf97d
ThinkingDobby <chiho1221@naver.com>
2021-11-11 21:40:06 +0900

    기초 디자인 구현 완료
    <FIN>
    기본 Activity 추가 - FindPetActivity, FindPersonActivity, HomeActivity
    pet_card 추가
    <TODO>
    RecyclerView 구현 - Adapter, ViewHolder 추가

---
+++ app/src/main/java/com/thinkingdobby/databaseproject/HomeActivity.kt
@@ -0,0 +1,23 @@
+package com.thinkingdobby.databaseproject
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        window.apply {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                statusBarColor = Color.TRANSPARENT
+            }
+        }
+    }
+}
